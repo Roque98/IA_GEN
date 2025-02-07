7bae175156f7c4538fa70487610d1fe8f93f4d56
Angel David Roque Ayala <ingroque98@gmail.com>
2025-02-07 13:09:58 -0600

    Se agrega la propiedad arhivoVisible en la entidad del archivo

--- Console/FolderViewWeb/Dapper/CodeGenerator/Entidades/CodeGeneratorArchivoEntidad.cs
+++ Console/FolderViewWeb/Dapper/CodeGenerator/Entidades/CodeGeneratorArchivoEntidad.cs
@@ -12,6 +12,7 @@
         public DateTime FechaCreacion { get; set; }
         public int? IdArchivoPadre { get; set; }
         public int idPromptTemplate { get; set; }
+        public bool archivoVisible { get; set; }
         public CodeGeneratorArchivoEntidad ArchivoPadre { get; set; }
         public ProyectoEntidad Proyecto { get; set; }
     }
